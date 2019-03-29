5ff42c2a526aad65d507f1ba39f2373175d582ac
Marcus Hellmann <hellmann@sub.uni-goettingen.de>
2019-03-29 13:31:34 +0100

    Optimized Subnavigation

--- Resources/Public/JavaScript/Dist/scripts.js
+++ Resources/Public/JavaScript/Dist/scripts.js
@@ -4,3 +4,28 @@
  * Licensed under the GPL-2.0-or-later license
  */
 
+$(function () {
+
+    $(window).on('resize, scroll', function () {
+
+        var heightDocument = $(document).height();
+        var heightViewport = $(window).height();
+        var heightHeader = $('#page-header').height();
+        var heightFooterHalf = $('#page-footer').height() * 0.2;
+
+        var scrollHeight = heightDocument - heightViewport - heightFooterHalf;
+
+        var headerScrolled = $(window).scrollTop();
+
+        if (headerScrolled > 150) {
+            $('.subnav-wrap').css('padding-top', headerScrolled-heightHeader);
+            if (headerScrolled > scrollHeight) {
+                $('.subnav-wrap').css('padding-top', scrollHeight-heightHeader);
+            }
+        } else {
+            $('.subnav-wrap').css('padding-top', headerScrolled);
+        }
+
+    });
+
+});
