cd8948ce94e21f6d63ab35ea95b88773afd02495
Marcus Hellmann <hellmann@sub.uni-goettingen.de>
2019-04-15 13:05:29 +0200

    Fixed Scrolling

--- Resources/Public/JavaScript/Dist/scripts.js
+++ Resources/Public/JavaScript/Dist/scripts.js
@@ -8,22 +8,50 @@ $(function () {
 
     var heightDocument = $(document).height();
     var heightViewport = $(window).height();
+    var heightHeader = $('#page-header').height();
+    var heightBreadcrumbs = $('.breadcrumb-section').height();
+    var heightPageContentInit = $('#page-content').height();
+    var heightFooterOuter = $('#page-footer').outerHeight();
     var heightFooterHalf = Math.trunc($('#page-footer').height() * 0.2);
+
+    /* minimale Höhe für Page Content */
+    var heightPageContentMin = heightViewport - heightHeader - heightBreadcrumbs - heightFooterOuter;
+    if (heightPageContentInit < heightPageContentMin) {
+        $('#page-content').css('min-height', heightPageContentMin);
+    }
 
     var maxPaddingTop = heightDocument - heightViewport - heightFooterHalf;
 
     $(window).on('resize, scroll', function () {
 
         var heightHeader = $('#page-header').height();
-        var headerScrolled = $(window).scrollTop();
 
-        if (headerScrolled > 150) {
-            $('.subnav-wrap').css('padding-top', headerScrolled-heightHeader);
-            if (headerScrolled > maxPaddingTop) {
-                $('.subnav-wrap').css('padding-top', maxPaddingTop-heightHeader);
+        /* Padding für Subnav Bar */
+        var isSubnavWrap = ($('.subnav-wrap').css('display'));
+
+        if ( isSubnavWrap !== 'none' ) {
+            var heightSubnavWrapOuter = $('.subnav-wrap').outerHeight();
+            if ( heightSubnavWrapOuter <= heightPageContentInit ) {
+                var headerScrolled = $(window).scrollTop();
+                if ( headerScrolled > 150 ) {
+                    $('.subnav-wrap').css('padding-top', headerScrolled - heightHeader);
+                    if (headerScrolled > maxPaddingTop) {
+                        $('.subnav-wrap').css('padding-top', maxPaddingTop - heightHeader + 50);
+                    }
+                } else {
+                    $('.subnav-wrap').css('padding-top', headerScrolled);
+                }
+            } else {
+                var headerScrolled = $(window).scrollTop();
+                if ( headerScrolled <= 50 ) {
+                    $('.subnav-wrap').css('padding-top', '0');
+                } else if ( (headerScrolled > 50) && (headerScrolled <= 150) ) {
+                    $('.subnav-wrap').css('padding-top', headerScrolled - 50);
+                } else {
+                }
             }
         } else {
-            $('.subnav-wrap').css('padding-top', headerScrolled);
+            $('.subnav-wrap').css('padding-top', '0');
         }
 
     });
